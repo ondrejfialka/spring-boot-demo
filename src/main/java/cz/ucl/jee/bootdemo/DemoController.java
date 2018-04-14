86a86d8b9a106d27d5e2f4ff92a384c2a4c5eb2c
Ondrej Fialka <ondrej.fialka@unicorncollege.cz>
2018-04-14 19:32:28 +0200

    added autowired emailService

--- src/main/java/cz/ucl/jee/bootdemo/DemoController.java
+++ src/main/java/cz/ucl/jee/bootdemo/DemoController.java
@@ -1,11 +1,17 @@
 package cz.ucl.jee.bootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
+	@Autowired
+	@Qualifier("emailerService")
+	EmailService emailService;
 	
 	 @RequestMapping(value = "/echo", method = RequestMethod.GET)
 	 public String echo() {
